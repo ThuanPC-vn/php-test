466969cafa18b47452c10322161838c4bc5098a7
ThuanPC <thuannhuynnh@gmail.com>
2024-08-19 17:01:41 +0700

    update html - remove sponsor menu brand - done

--- MVC_UX&UI/app/views/includes/public_sponsor.php
+++
@@ -1,9 +0,0 @@
-<!--==================== SPONSOR ====================-->
-            <section class="sponsor section">
-               <div class="sponsor__container container grid">
-                  <img src="resources/img/sponsor-1.png" alt="image" class="sponsor__img">
-                  <img src="resources/img/sponsor-2.png" alt="image" class="sponsor__img">
-                  <img src="resources/img/sponsor-3.png" alt="image" class="sponsor__img">
-                  <img src="resources/img/sponsor-4.png" alt="image" class="sponsor__img">
-               </div>
-            </section>
